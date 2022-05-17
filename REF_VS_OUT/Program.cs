ac0d385c717f54e66e1995425c3da2a9dddcae31
IRMuhammadjon <102463648+IRMuhammadjon@users.noreply.github.com>
2022-05-17 10:54:28 +0500

    Add project files.

---
+++ REF_VS_OUT/Program.cs
@@ -0,0 +1,31 @@
+﻿// C# program to illustrate the
+// concept of ref parameter
+using System;
+
+class GFG
+{
+
+	
+	public static void Main()
+	{
+
+		
+		string str = "muhammadjon";
+
+	
+		SetValue(ref str);
+
+		Console.WriteLine(str);
+	}
+
+	static void SetValue(ref string str1)
+	{
+
+		if (str1 == "muhamamdjon")
+		{
+			Console.WriteLine("Salom! MUhammadjon");
+		}
+
+		str1 = "Rahmatullayev ";
+	}
+}
